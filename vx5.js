fcbcef589bea48118ac423c0c9414cdff3262e7f
MahmoudvMarey <138885244+MahmoudvMarey@users.noreply.github.com>
2024-10-22 13:21:43 +0300

    Update vx5.js

--- vx5.js
+++ vx5.js
@@ -3,7 +3,7 @@ function main() {
   let iframe = document.createElement("iframe");
   iframe.src = `https://virsay.com/embed/org_01JAGJXKSYWYHWTXP4FZ0DBCPP`;
   iframe.style.position = "fixed";
-  iframe.style[position] = "1rem"; // position value = "right"
+  iframe.style.right = "1rem"; // Correctly setting right position
   iframe.style.bottom = "5.25rem"; // Equivalent to Tailwind's bottom-4
   iframe.style.width = "382px"; // Equivalent to Tailwind's w-full
   iframe.style.height = "calc(100% - 5.25rem)";
